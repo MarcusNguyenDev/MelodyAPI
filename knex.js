50ed3086fb996140b32cbabeeb4b5b11166597ec
Marcus Nguyen <marcus.kimlong@gmail.com>
2023-01-19 16:29:32 +1000

    fix

---
+++ knex.js
@@ -0,0 +1,21 @@
+module.exports = {
+  client: "mysql2",
+  connection: {
+    host: "localhost",
+    database: "melodydb",
+    port: 3306,
+    user: "admin",
+    password: "ThisIsAnAdminPassword",
+  },
+};
+
+// module.exports = {
+//     client: 'mysql2',
+//     connection: {
+//         host: 'localhost',
+//         database: 'melodydb',
+//         port: 3306,
+//         user: 'root',
+//         password: 'root'
+//         }
+// };
