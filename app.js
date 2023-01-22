b8d036c764dc2326cbb981571329c40d0572bd0f
Marcus Nguyen <marcus.kimlong@gmail.com>
2023-01-22 16:24:26 +1000

    Update app.js

--- app.js
+++ app.js
@@ -81,6 +81,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("sitemap.xml"));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
