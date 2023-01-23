aa3296ceea18696254d8f2c08b63e99e0e26f95f
Marcus Nguyen <marcus.kimlong@gmail.com>
2023-01-24 02:13:35 +1000

    Update gallery.js

--- routes/gallery.js
+++ routes/gallery.js
@@ -19,6 +19,7 @@ router.get("", (req, res, next) => {
   req.db
     .select("*")
     .from("gallery")
+    .orderBy("Id", "desc")
     .then((data) => res.status(200).json(data))
     .catch((err) => {
       console.log(err),
